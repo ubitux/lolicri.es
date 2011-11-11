c39be56e875f99f73c6e7178ba4c5f495f366146
Clément Bœsch <ubitux@gmail.com>
2011-11-11 11:12:23 +0100

    Initial PHP version.

---
+++ www/index.php
@@ -0,0 +1,255 @@
+<?php
+
+$l = array(
+	array(
+		'name'	=> 'Misuzu Kamio',
+		'anime'	=> 'Air',
+		'pic'	=> 'img/Air-Misuzu-Kamio.png',
+		'cries'	=> array(
+			array('Gao', 'snd/Air-Misuzu-Kamio-Gao.ogg'),
+			array('Nihaha', 'snd/Air-Misuzu-Kamio-Nihaha.ogg'),
+			array('V!', 'snd/Air-Misuzu-Kamio-V.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Lucy',
+		'anime'	=> 'Elfen Lied',
+		'pic'	=> 'img/Elfen-Lied-Nyu.png',
+		'cries'	=> array(
+			array('Nyu!', 'snd/Elfen-Lied-Nyu.ogg'),
+			array('Nyuuu', 'snd/Elfen-Lied-Nyuuu.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Rika Furude',
+		'anime'	=> 'Higurashi no Naku Koro ni',
+		'pic'	=> 'img/Higurashi-Rika.png',
+		'cries'	=> array(
+			array('Ni-pah', 'snd/Higurashi-Rika-Furude-Ni-pah.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Megumi Noda',
+		'anime'	=> 'Nodame Cantabile',
+		'pic'	=> 'img/Nodame-Cantabile-Megumi-Noda.png',
+		'cries'	=> array(
+			array('Gyabou!', 'snd/Nodame-Cantabile-Megumi-Noda-Gyabou.ogg'),
+			array('Mugii!', 'snd/Nodame-Cantabile-Megumi-Noda-Mugii.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Ahiru',
+		'anime'	=> 'Princess Tutu',
+		'pic'	=> 'img/Princess-Tutu-Ahiru.png',
+		'cries'	=> array(
+			array('Quack!', 'snd/Princess-Tutu-Ahiru-Quack.ogg'),
+			array('Quack! Quack! Quack!', 'snd/Princess-Tutu-Ahiru-Quack-Quack-Quack.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Churuya',
+		'anime'	=> 'Nyoro~n Churuya-san',
+		'pic'	=> 'img/Nyoro-n-Churuya.png',
+		'cries'	=> array(
+			array('Nyoro~n', 'snd/Nyoro-n-Churuya-Nyoro-n.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Chie',
+		'anime'	=> 'Eve no Jikan',
+		'pic'	=> 'img/Eve-ni-Jikan-Chie.png',
+		'cries'	=> array(
+			array('Nya', 'snd/Eve-no-Jikan-Chie-Nya.ogg'),
+			array('Nya! Nya!', 'snd/Eve-no-Jikan-Chie-Nya-nya.ogg'),
+			array('Neko dano!', 'snd/Eve-no-Jikan-Chie-Neko-dano.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Yurie',
+		'anime'	=> 'Kamichu!',
+		'pic'	=> 'img/Kamichu-Yurie.png',
+		'cries'	=> array(
+			array('Ka-Mi-Chu!', 'snd/Kamichu-Yurie-Ka-Mi-Chu.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Mii',
+		'anime'	=> 'Popotan',
+		'pic'	=> 'img/Popotan-Mii.png',
+		'cries'	=> array(
+			array('Puni Puni Da !', 'snd/Popotan-Mii-Puni-Puni.ogg'),
+			array('Pikohan Magicalu, Parareru Magicalu', 'snd/Popotan-Mii-Pikohan-Magicalu-Parareru-Magicalu.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Himeko',
+		'anime'	=> 'Paniponi Dash!',
+		'pic'	=> 'img/Paniponi-Dash-Himeko.png',
+		'cries'	=> array(
+			array('Maho', 'snd/Paniponi-Dash-Himeko-Maho.ogg'),
+			array('Kawaii', 'snd/Paniponi-Dash-Himeko-Kawaii.ogg'),
+			array('Omega Kawaii', 'snd/Paniponi-Dash-Himeko-Omega-Kawaii.ogg'),
+		),
+	),
+	/*array(
+		'name'	=> 'Sakura',
+		'anime'	=> 'Cardcaptor Sakura',
+		'pic'	=> 'img/Cardcaptor-Sakura-Sakura.png',
+		'cries'	=> array(
+			array('Release !', 'snd/Cardcaptor-Sakura-Sakura-Release.ogg'),
+			array('Claw Card !', 'snd/Cardcaptor-Sakura-Sakura-Claw-Card.ogg'),
+		),
+	),*/
+	array(
+		'name'	=> 'Mao Onigawara',
+		'anime'	=> 'Ground Defence Mao-chan',
+		'pic'	=> 'img/Mao-chan-Mao-Onigawara.png',
+		'cries'	=> array(
+			array('Ah-uuh-e-uuh', 'snd/Mao-chan-Mao-Onigawara-AUEU.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Kyouko Okonogi',
+		'anime'	=> 'Dennou Coil',
+		'pic'	=> 'img/Dennou-Coil-Kyouko.png',
+		'cries'	=> array(
+			array('Unchi !', 'snd/Dennou-Coil-Kyouko-Unchi.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Dokuro Mitsukai',
+		'anime'	=> 'Bokusatsu Tenshi Dokuro-chan',
+		'pic'	=> 'img/Bludgeoning-Angel-Dokuro-Chan.png',
+		'cries'	=> array(
+			array('Pipiru piru piru pipiru pi !', 'snd/Bludgeoning-Angel-Dokuro-Chan-Pipirupirupirupipirupi.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Yuu Morisawa',
+		'anime'	=> 'Mahou no Tenshi Creamy Mami',
+		'pic'	=> 'img/Creamy-Mami-Yuu.png',
+		'cries'	=> array(
+			array('Pampuru pimpuru pampoppun! Pimpuru pampuru pampoppun!', 'snd/Creamy-Mami-Yuu-Pampuru-pimpuru-pampoppun.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Makoto Sawatari',
+		'anime'	=> 'Kanon',
+		'pic'	=> 'img/Kanon-Makoto-Sawatari.png',
+		'cries'	=> array(
+			array('Auu... Auu! Auuu...', 'snd/Kanon-Makoto-Sawatari-Auu-Auu-Auu.ogg'),
+			array('Auuuu! Auu...', 'snd/Kanon-Makoto-Sawatari-Auu-Auu.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Ayu Tsukimiya',
+		'anime'	=> 'Kanon',
+		'pic'	=> 'img/Kanon-Ayu-Tsukimiya.png',
+		'cries'	=> array(
+			array('Uguu', 'snd/Kanon-Ayu-Tsukimiya-Uguu.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Rena Ryuuguu',
+		'anime'	=> 'Higurashi no Naku Koro ni Kai',
+		'pic'	=> 'img/Higurashi-Rena-Ryuuguu.png',
+		'cries'	=> array(
+			array('Kawaii', 'snd/Higurashi-Rena-Ryuuguu-Kawaii.ogg'),
+			array('Omochi Kaeri', 'snd/Higurashi-Rena-Ryuuguu-Omochi-Kaeri.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Kasumi Toomine',
+		'anime'	=> 'Alien Nine',
+		'pic'	=> 'img/Alien_9-Toomine_Kasumi.png',
+		'cries'	=> array(
+			array('Nya', 'snd/Alien_9-Toomine_Kasumi-Nya.ogg'),
+			array('Nya~a', 'snd/Alien_9-Toomine_Kasumi-Nyaaa.ogg'),
+		),
+	),
+	array(
+		'name'	=> 'Kagura',
+		'anime'	=> 'Azumanga Daioh',
+		'pic'	=> 'img/Azumanga_Daioh-Kagura.png',
+		'cries'	=> array(
+			array('Garrr', 'snd/Azumanga_Daioh-Kagura-Garrr.ogg'),
+		),
+	),
+	array(
+		'name'  => 'Pino',
+		'anime' => 'Ergo Proxy',
+		'pic'   => 'img/Ergo_Proxy-Pino.png',
+		'cries' => array(
+			array('Ehehee', 'snd/Ergo_Proxy-Pino-Ehehee.ogg'),
+		),
+	),
+	array(
+		'name'  => 'Mayuri',
+		'anime' => 'Steins;Gate',
+		'pic'	=> 'img/Steins_Gate-Mayuri.png',
+		'cries' => array(
+			array('Tutuluu', 'snd/Steins_Gate-Mayuri-Tutuluu.ogg'),
+		),
+	),
+);
+
+?><!DOCTYPE html>
+<html>
+<?php if (preg_match('/facebook.com/', $_SERVER['HTTP_REFERER']) || preg_match('/facebook.com/', $_SERVER['HTTP_USER_AGENT'])) { ?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>No facebook.</title>
+		<link rel="icon" type="image/png" href="favicon.png" />
+		<style type="text/css">
+body { color: #C0C0C0; background-color: #101010; font-family: "sans-serif"; }
+div#main { width: 400px; border: 4px solid white; background-color: #202020; margin: auto; margin-top: 50px; padding: 20px; }
+		</style>
+	</head>
+	<body>
+		<div id="main">
+			<p>Hi,</p>
+			<p>
+				Please don't use facebook to come here.  Facebook is centralized, closed,
+				commercial, harmful, threatening freedom and certainly not essential as a
+				communication vehicle.
+			</p>
+			<p>Thanks.<p>
+			<hr />
+			<p>Salut,</p>
+			<p>
+				Merci de ne pas passer par facebook pour venir ici.  Facebook est centralisé
+				fermé, commercial, nuisible, liberticide et certainement pas indispensable en
+				tant que vecteur de communication.
+			</p>
+			<p>Merci.</p>
+		</div>
+<?php } else { ?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Loli Cries!</title>
+		<meta name="viewport" content="width=device-width" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link rel="icon" type="image/png" href="favicon.png" />
+	</head>
+	<body>
+		<h1><span class="h">❤</span> Loli Cries! <span class="h">❤</span></h1>
+		<div id="m">
+<?php
+	foreach ($l as $loli) {
+		$phpsux = split(' ', $loli['name']);
+		$key = strtolower($phpsux[0]);
+		echo "\t\t\t<div class=\"i\" id=\"$key\">\n\t\t\t\t<h2>$loli[name] (<i>$loli[anime]</i>)</h2>\n\t\t\t\t<img src=\"$loli[pic]\" />";
+		foreach ($loli['cries'] as $cry)
+			echo "\n\t\t\t\t<div class=\"c\">$cry[0]</div> <audio src=\"$cry[1]\" controls=\"controls\"></audio><br />";
+		echo "\n\t\t\t</div>\n";
+	}
+	echo "\t\t\t<div style=\"clear:left;\"></div>\n\t\t</div>\n";
+}
+?>
+		<footer>
+			<p class="csr">Comment/Submit/Request: #/dev/null @ irc.yozora-irc.net</p>
+			<p>2010-2011 - The loli ❤ team</p>
+		</footer>
+	</body>
+</html>
+
