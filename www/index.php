494e7a60858d1877fbb35e0b9711caf546c4d316
Thibault Billon <thb@xiun.org>
2011-11-11 11:39:39 +0100

    Port style and new layout from dead django version.

--- www/index.php
+++ www/index.php
@@ -8,23 +8,38 @@
 		<link rel="icon" type="image/png" href="favicon.png" />
 	</head>
 	<body>
-		<h1><span class="h">❤</span> Loli Cries! <span class="h">❤</span></h1>
-		<div id="m">
-<?php
-	foreach ($lolis as $loli) {
-		$phpsux = split(' ', $loli['name']);
-		$key = strtolower($phpsux[0]);
-		echo "\t\t\t<div class=\"i\" id=\"$key\">\n\t\t\t\t<h2>$loli[name] (<i>$loli[anime]</i>)</h2>\n\t\t\t\t<img src=\"$loli[pic]\" />";
-		foreach ($loli['cries'] as $cry)
-			echo "\n\t\t\t\t<div class=\"c\">$cry[0]</div> <audio src=\"$cry[1]\" controls=\"controls\"></audio><br />";
-		echo "\n\t\t\t</div>\n";
-	}
-	echo "\t\t\t<div style=\"clear:left;\"></div>\n\t\t</div>\n";
-?>
+
+	<header>
+	  <hgroup>
+	    <h1>Loli Cries!</h1>
+	    <h2>The internet loli database</h2>
+	  </hgroup>
+	</header>
+
+	<section id="lolis">
+	  <?php foreach ($lolis as $l) { ?>
+	  <article>
+	    <header>
+	      <hgroup>
+	        <h1><?php echo $l['name'] ?></h1>
+	        <h2><?php echo $l['anime'] ?></h2>
+	      </hgroup>
+	    </header>
+
+	    <img src="<?php echo $l['pic']; ?>" />
+	    <dl>
+	      <?php foreach ($l['cries'] as $cry) {?>
+	      <dt><?php echo $cry[0] ?></dt>
+	      <dd><audio preload="none" src="<?php echo $cry[1] ?>" controls="controls"></audio></dd>
+	      <?php } ?>
+	    </dl>
+	  </article>
+	  <?php } ?>
+	</section>
+
 		<footer>
-			<p class="csr">Comment/Submit/Request: #/dev/null @ irc.yozora-irc.net</p>
-			<p>2010-2011 - The loli ❤ team</p>
+			<p>Comment/Submit/Request: #/dev/null @ irc.yozora-irc.net</p>
+			<p class="loli❤">2010-2011 - The loli ❤ team</p>
 		</footer>
 	</body>
 </html>
-
