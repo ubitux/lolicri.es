67e65994608fde2f605ac7357aa3c1560ecad78a
db0 <db0company@gmail.com>
2012-03-22 15:51:55 +0100

    new loli: Hinata from Ro-Kyu-Bu
    Both cries are extracted from episode 7 (Ooh~ around 6:50 and
    Koala-attack~ around 11:15).

--- loli_list.py
+++ loli_list.py
@@ -226,4 +226,12 @@ lolis = [{
     'cries': [
         ('Gao', 'snd/Touch-Minami-Asakura-Gao.ogg'),
     ],
+},{
+    'name': 'Hinata Hakamada',
+    'anime': 'Ro-Kyu-Bu',
+    'pic': 'img/Ro-Kyu-Bu-Hinata.png',
+    'cries': [
+        ('Ooh',          'snd/Ro-Kyu-Bu-Hinata-Ooh.ogg'),
+        ('Koala Attack', 'snd/Ro-Kyu-Bu-Hinata-Koala-Attack.ogg'),
+    ],
 }]
