3251ef1b6f37194a6a84c1022fc77278b1978513
Thibault Billon <thb@xiun.org>
2013-05-28 13:38:22 +0200

    new granny: Misato Katsuragi from Evangelion
    Both the cry and the picture come from episode 2 at around 13:08.

--- granny_list.py
+++ granny_list.py
@@ -1,3 +1,10 @@
 # vim: set et sts=4 sw=4:
 
-grannies = []
+grannies = [{
+    'name': 'Misato Katsuragi',
+    'anime': 'Neon Genesis Evangelion',
+    'pic': 'img/Evangelion-Misato-Katsuragi.png',
+    'cries': [
+        ('Kaaaa~', 'snd/Evangelion-Misato-Katsuragi-Kaaa.ogg'),
+    ],
+}]
